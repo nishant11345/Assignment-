11b249fadd73d7d492a8764d592fa7cf85f2ad38
Nishant <109360944+nishant11345@users.noreply.github.com>
2024-06-27 15:37:18 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,31 @@
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active-slide');
+        if (i === index) {
+            slide.classList.add('active-slide');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 1000);
+
+showSlide(currentSlide);
